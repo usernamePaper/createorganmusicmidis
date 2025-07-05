6a01fc464382074d4a2b2c4272005a97389406aa
usernamePaper <99168593+usernamePaper@users.noreply.github.com>
2025-07-05 23:11:58 +0400

    Update play2.lua

--- play2.lua
+++ play2.lua
@@ -62,7 +62,7 @@ for i = 1, #files do
         :setSize(30,3)
         :onClick(function()
         basalt.stop()
-        shell.run("/play", fs.combine("/music/", fs.getName(files[i])))
+        shell.execute("/play", fs.combine("/music/", fs.getName(files[i])))
         
         end)
         
